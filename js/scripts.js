5b7323cb8f612c41509e67f74feebe03b1daa82a
ardralak <ardralak10@gmail.com>
2024-05-14 21:06:57 +0530

    Changes for Ardra and Mithun

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'e1bf9e2a8e5fea6249935b940842b271'
+            && MD5($('#invite_code').val()) !== '1649e93fee3f12072127709c518c0912') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyXtHJRbvdjWf0QEFELHGtncNbrWoPN9H9lkJVpDgLk-3ai7AYMolMA0W_rtN5-BQm21w/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 9.63847, lng: 76.54750};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
